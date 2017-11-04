c0707287a963a70a685d621316470c98cb261d7b
Jonnie Clermont <clermontster@gmail.com>
2017-11-04 11:53:06 -0500

    added working http request, env, updated gitignore

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
+require('dotenv').config()
+const {
+	API_KEY,
+	CUSTOM_SEARCH_ID,
+} = process.env;
+console.log(process.env);
 const express = require('express');
 const app = express();
 const https = require('https');
+// const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const ENDPOINT='www.googleapis.com'
 const PATH='/customsearch/v1';
-const API_KEY='?key=AIzaSyBh0fQ9Sy4Ly_w5Qlrv8aakh4GtxHaVeEo';
-const CUSTOM_SEARCH_ID='&cx=009228922841197772232:ytpzunobqs4';
+
 const QUERY_PREFIX='&q=';
 
+let body ='';
+
+function returnData(data) {
+	const body = JSON.parse(data);
+	const items = body.items.map((item) => {
+		return ({
+			url: item.link,
+    	snippet: item.title,
+    	thumbnail: item.pagemap.cse_thumbnail,
+    	context:item.formattedUrl,
+		});
+	});
+	return items;
+}
 
 app.get('/:query', (req, res) => {
-	if (req.params.length) {
-		res.send(req.params.query);
+	if(req.params.query === 'favicon.ico'){
+		return;
 	}
-
 	const options = {
 	  host: ENDPOINT,
 	  path: `${PATH}${API_KEY}${CUSTOM_SEARCH_ID}${QUERY_PREFIX}${req.params.query}`,
-	  prettyPrint: true,
-	  searchType: 'image',
-	  cr: 'countryUS',
 	};
-
-	const imageRequest = https.get(options, (response) => {
-	 	let body;
- 		response.setEncoding('utf8');
-
-	  response.on('data', (data) => {
-	  	body += data;
-	  }).on('end', () => {
-	  	// console.log(body['items']);
-	  	// const results = body.items.reduce((results, item) =>{
-	  	// 	return {
-	  	// 		url: item.link,
-	  	// 		snippet: item.title,
-	  	// 		thumbnail: item.pagemap.cse_thumbnail,
-	  	// 		context:item.formatted.url,
-	  	// 	};
-	  	// });
-	  	 // const responseBody = { headers, method, url, body };
-
-    	// res.write(JSON.stringify(responseBody));
-	  	res.setHeader('Content-Type', 'application/json');
-
-	  	res.send(body);
+	const url = 'http://data.cityofchicago.org/resource/6zsd-86xi.json';
+	var request = https.get(options, (response) => {
+		response.setEncoding('utf8');
+	  response.on('data', function(data) {
+	  	body+=data.toString();
 	  });
-	});
 
-	imageRequest.on('error', (e)=> {
-		console.log('Request error:', e.message);
+	  response.on('end', function() {
+	  	// const parsed = JSON.parse(body)[2];
+	  	const items = returnData(body);
+	  	res.send(items);
+		});
 	});
-
-	imageRequest.end();
+	request.end();
+	
+	
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
