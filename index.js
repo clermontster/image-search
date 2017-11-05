22c6526a60ab66725fa1f6c9957e9b639afc4766
Jonnie Clermont <clermontster@gmail.com>
2017-11-05 13:36:39 -0600

    fixed a bug throwing error when back to back requests sent

--- index.js
+++ index.js
@@ -1,59 +1,69 @@
 require('dotenv').config()
 const {
-	API_KEY,
-	CUSTOM_SEARCH_ID,
+  API_KEY,
+  CUSTOM_SEARCH_ID,
 } = process.env;
-console.log(process.env);
 const express = require('express');
 const app = express();
-const https = require('https');
+const https = require('http');
 // const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const ENDPOINT='www.googleapis.com'
 const PATH='/customsearch/v1';
-
+const FIELDS='&fields=items(link,title,formattedUrl,pagemap/cse_thumbnail/src)';
 const QUERY_PREFIX='&q=';
 
-let body ='';
+function getThumbnail(item) {
+  return item.pagemap ? item.pagemap.cse_thumbnail[0].src : '';
+} 
 
 function returnData(data) {
-	const body = JSON.parse(data);
-	const items = body.items.map((item) => {
-		return ({
-			url: item.link,
-    	snippet: item.title,
-    	thumbnail: item.pagemap.cse_thumbnail,
-    	context:item.formattedUrl,
-		});
-	});
-	return items;
+  body = JSON.parse(data);
+  console.log(typeof body);  
+  const items = body.map((item) => {
+    return ({
+      location: item.location,
+      location_description: item.location_description,
+      primary_type: item.primary_type,
+    });
+  });
+  //   const items = body.items.map((item) => {
+  //   return ({
+  //     url: item.link,
+  //     snippet: item.title,
+  //     thumbnail: getThumbnail(item),
+  //     context:item.formattedUrl,
+  //   });
+  // });
+  return items;
 }
 
-app.get('/:query', (req, res) => {
-	if(req.params.query === 'favicon.ico'){
-		return;
-	}
-	const options = {
-	  host: ENDPOINT,
-	  path: `${PATH}${API_KEY}${CUSTOM_SEARCH_ID}${QUERY_PREFIX}${req.params.query}`,
-	};
-	const url = 'http://data.cityofchicago.org/resource/6zsd-86xi.json';
-	var request = https.get(options, (response) => {
-		response.setEncoding('utf8');
-	  response.on('data', function(data) {
-	  	body+=data.toString();
-	  });
+app.get('/:search', (req, res) => {
+  const options = {
+    host: ENDPOINT,
+    path: `${PATH}${API_KEY}${CUSTOM_SEARCH_ID}${FIELDS}${QUERY_PREFIX}${req.params.search}`,
+  };
+  let body ='';
 
-	  response.on('end', function() {
-	  	// const parsed = JSON.parse(body)[2];
-	  	const items = returnData(body);
-	  	res.send(items);
-		});
-	});
-	request.end();
-	
-	
+
+  if(req.params.search === 'favicon.ico' || req.query.offet === 'favicon.ico'){
+    return;
+  }
+
+  const url = 'http://data.cityofchicago.org/resource/6zsd-86xi.json';
+  var request = https.get(url, (response) => {
+    res.setHeader('Content-Type', 'application/json')
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      body+=data;
+    });
+
+    response.on('end', function() {
+      const items = returnData(body);
+      res.send(items);
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
